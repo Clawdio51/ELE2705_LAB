da3f93782714ee420d78dab1d24e74efce103b0a
Claudio Alkazzi <clawdio51@gmail.com>
2024-01-14 23:35:31 -0500

    Port forwarding coded

--- client/test/connect_to_server.py
+++ client/test/connect_to_server.py
@@ -1,5 +1,6 @@
 import socket
 import json
+import time
 
 
 HOST = "132.207.64.218"  # The server's hostname or IP address
@@ -7,9 +8,11 @@ PORT = 54654  # The port used by the server
 
 config = {}
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    response = str({"status": "OK", "data": "data"})
-    s.sendto(json.dumps(response).encode(), (HOST, PORT))
+    while True: 
+        response = str({"status": "OK", "data": "data"})
+        s.sendto(json.dumps(response).encode(), (HOST, PORT))
+        time.sleep(2)
 
     # Print server echo reply
-    reply, addr = s.recvfrom(1024)
-    print(reply.decode())
+    # reply, addr = s.recvfrom(1024)
+    # print(reply.decode())
--- client/test/port_forwarding.py
+++ client/test/port_forwarding.py
@@ -1,13 +1,41 @@
 import socket
+import threading
 
-HOST = "192.168.7.1"
-PORT = 54654
+# TODO: Might need to remove multithreading and directly wait for reply from server and forward to client after the initial forward
 
-DEST_HOST = "192.168.0.1"
-DEST_PORT = 12345
+# Forward to server
+S_LISTEN_ADDRESS    = "192.168.7.1"
+S_LISTEN_PORT       = 54654
+S_CONNECT_ADDRESS   = "192.168.7.1"
+S_CONNECT_PORT      = 12345
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    s.bind((HOST, PORT))
-    while True:
-        data, addr = s.recvfrom(1024)
-        s.sendto(data, (DEST_HOST, DEST_PORT))
+# Forward to client
+C_LISTEN_ADDRESS    = "192.168.0.1"
+C_LISTEN_PORT       = 12345
+C_CONNECT_ADDRESS   = "192.168.7.2"
+C_CONNECT_PORT      = 54654
+
+def forward(listenaddress, listenport, connectaddress, connectport):
+
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as private_socket:
+        private_socket.bind((listenaddress, listenport))
+        while True:
+
+            data, client_addr = private_socket.recvfrom(1024)
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as public_socket:
+                public_socket.sendto(data, (connectaddress, connectport))
+                response, server_addr = public_socket.recvfrom(1024)
+            private_socket.sendto(response, client_addr)
+
+if __name__ == "__main__":
+
+    forward(S_LISTEN_ADDRESS, S_LISTEN_PORT, S_CONNECT_ADDRESS, S_CONNECT_PORT)
+
+    # t1 = threading.Thread(target=forward, args=(S_LISTEN_ADDRESS, S_LISTEN_PORT, S_CONNECT_ADDRESS, S_CONNECT_PORT))
+    # t2 = threading.Thread(target=forward, args=(C_LISTEN_ADDRESS, C_LISTEN_PORT, C_CONNECT_ADDRESS, C_CONNECT_PORT))
+
+    # t1.start()
+    # t2.start()
+    
+    # t1.join()
+    # t2.join()
